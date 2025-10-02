03457c2572ad2a9046b09269d303d50706b9528a
Zhanaiym444 <zhanaiymzhagparova@gmail.com>
2025-10-02 10:02:30 +0500

    Add files via upload

---
+++ LAB3.2.py
@@ -0,0 +1,12 @@
+
+import math
+class Shape:
+    def area(self):
+        return 0
+
+class Square(Shape):
+    def __init__(self, length):
+        self.length = length
+
+    def area(self):
+        return self.length ** 2
